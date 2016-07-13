80e61dc29def85b053b5da87f597a8d569f943a8
zhangkaihongzkh <953628520@qq.com>
2016-07-13 10:25:30 +0800

    增加了连缀功能 核心是创建elements来保存Base对象中属性数组 同时通过var $来新建对象 保证每次操作对象的独立性

--- js/base.js
+++ js/base.js
@@ -1,4 +1,4 @@
-var Base = {
+/*var Base = {
 	getId:function(id){
 		return document.getElementById(id);
 	},
@@ -8,4 +8,49 @@ var Base = {
 	getTagName:function(tagName){
 		return document.getElementsByTagName(tagName);
 	}
+}*/
+var $ = function(){
+	return new Base();
 }
+function Base(){
+	this.elements = [];	//把返回的节点对象保存在Base对象的属性数组中
+
+	//获取元素ID
+	this.getId = function(id){
+		this.elements.push(document.getElementById(id));
+		return this;
+	};
+
+	//获取标签名
+	this.getTagName = function(tag){
+		var tags = document.getElementsByTagName(tag);
+		for(var i = 0;i < tags.length;i++){
+			this.elements.push(tags[i]);
+		}
+		return this;
+	};
+};
+
+//设置css方法
+Base.prototype.css = function(attr,value){
+	for(var i = 0; i < this.elements.length;i++){
+		this.elements[i].style[attr] = value;
+	}
+	return this;
+};
+
+//HTML方法
+Base.prototype.html = function(str){
+	for(var i =0;i<this.elements.length;i++){
+		this.elements[i].innerHTML = str;
+	}
+	return this;
+};
+
+//点击事件
+Base.prototype.click = function(fn){
+	for(var i = 0;i<this.elements.length;i++){
+		this.elements[i].onclick = fn;
+	}
+	return this;
+};
--- js/test.js
+++ js/test.js
@@ -1,5 +1,15 @@
 window.onload = function(){
-	alert(Base.getId('box').innerHTML);
+	/*alert(Base.getId('box').innerHTML);
 	alert(Base.getName('sex')[0].value);
-	alert(Base.getTagName('p')[0].innerHTML);
-};
+	alert(Base.getTagName('p')[0].innerHTML);*/
+
+	
+/*	alert(base.getId('box').elements.length);
+	alert(base.getTagName('p').elements.length);*/
+
+	$().getId('box').html('abc');
+
+	$().getTagName('p').css('backgroundColor','green').html('123').click(function(){
+		alert('a')
+	});
+}; 
